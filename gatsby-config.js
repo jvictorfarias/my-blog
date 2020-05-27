392a64eb25ee8e6d250d92bf7ee721c4114004b4
jvictorfarias <victorfarias.new@gmail.com>
2020-05-27 08:53:17 -0300

    cleaning the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `My Blog`,
+    description: `A blog with posts about development made by Joao Victor Farias`,
     author: `@gatsbyjs`,
   },
   plugins: [
