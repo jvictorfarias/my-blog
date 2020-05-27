139a31716cb14ebd344955f06d5799ea62e2334e
jvictorfarias <victorfarias.new@gmail.com>
2020-05-27 14:05:00 -0300

    :recycle: using staticQuery hook

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,34 +1,33 @@
 import React from 'react'
 
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-      query ProfileMetadata {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-            position
-          }
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, author, description },
+    },
+  } = useStaticQuery(graphql`
+    query ProfileMetadata {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          position
         }
       }
-    `}
-    render={({
-      site: {
-        siteMetadata: { title, position, author, description },
-      },
-    }) => (
-      <div>
-        <h1>{title}</h1>
-        <h2>{author}</h2>
-        <span>{position}</span>
-        <p>{description}</p>
-      </div>
-    )}
-  />
-)
+    }
+  `)
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{author}</h2>
+      <span>{position}</span>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default Profile
