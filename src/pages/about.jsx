f669784fd68c45238173d8a8476eea64f30b95af
jvictorfarias <victorfarias.new@gmail.com>
2020-05-27 09:38:47 -0300

    :recycle: cleaning the template

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const AboutPage = () => (
+  <>
+    <h1>About Page</h1>
+    <ul>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: 'red' }}>Home</Link>
+      </li>
+    </ul>
+  </>
+)
+
+export default AboutPage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import { Link } from 'gatsby';
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: 'red' }}>Home</Link>
+      </li>
+    </ul>
 
   </Layout>
 )
